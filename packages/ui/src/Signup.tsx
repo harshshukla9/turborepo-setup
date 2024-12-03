0e9f39e13fc72511f0d288bce9cf40f6fe7fe20c
harshshukla9 <harshshukla9792@gmail.com>
2024-12-03 09:55:33 +0530

    first turbo

---
+++ packages/ui/src/Signup.tsx
@@ -0,0 +1,8 @@
+
+export const Signup = () => {
+    return <div style={{width:400,border:"1px solid black"}}>
+        <input type="text" placeholder="email"></input>
+        <input type="password" placeholder="passkey"></input>
+        <button>Submit</button>
+    </div>
+}
